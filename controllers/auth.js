af542e22f6bca1272188aa09cb2a726a7b3ba888
Undefined-user <undefinded-user@users.noreply.github.com>
2020-12-05 21:51:32 +0300

    create auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,19 @@
-exports.createOrUpdateUser = ( req, res) => {
-	res.json({
-		message: 'This is creare and update user endpoint'
-	})
+const User = require('../models/user')
+
+exports.createOrUpdateUser = async ( req, res) => {
+	const { name, picture, email } = req.user
+	// find user in firebase and update userinfo first arg - selector, second - new info, third - optional
+	const user = await User.findOneAndUpdate({email}, {name: email.split('@')[0] , picture}, {new:true})
+	if(user){
+		console.log('USER_UPDATED', user)
+		res.json(user)
+	} else {
+		const newUser = await new User({
+			email,
+			name : email.split('@')[0],
+			picture
+		}).save()
+		console.log('USER_CREATED', newUser)
+		res.json(newUser)
+	}
 }
