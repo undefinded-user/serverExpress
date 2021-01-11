e52fc37af257a6be6a80a8ca5578da81d3bff6f8
Undefined-user <undefinded-user@users.noreply.github.com>
2021-01-11 19:27:17 +0300

    Add prepopulated/product route

--- routes/product.js
+++ routes/product.js
@@ -3,13 +3,23 @@ const router = express.Router()
 const slugify = require('slugify')
 
 const { adminCheck, authCheck }  = require('../middlewares/auth')
-const { create, update, read, remove, list, listSorted, productsCount } = require('../controllers/product')
+const {
+	 create,
+	 update,
+	 read,
+	 populateAndRead,
+	 remove,
+	 list,
+	 listSorted,
+	 productsCount 
+} = require('../controllers/product')
 
 
 router.post('/product', authCheck, adminCheck, create)
 router.get('/products/total', productsCount)
 router.get('/products/:count', list)
 router.get('/product/:slug', read)
+router.get('/product/prepopulated/:slug', populateAndRead)
 router.put('/product/:slug', authCheck, adminCheck, update)
 router.delete('/product/:slug', authCheck, adminCheck, remove)
 
