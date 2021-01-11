c2011fe9f379ab96a99a2eebb9ae4881149a953a
Undefined-user <undefinded-user@users.noreply.github.com>
2021-01-11 09:16:20 +0300

    Product routes

---
+++ routes/product.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const slugify = require('slugify')
+
+const { adminCheck, authCheck }  = require('../middlewares/auth')
+const { create, update, read, remove, list, listSorted, productsCount } = require('../controllers/product')
+
+
+router.post('/product', authCheck, adminCheck, create)
+router.get('/products/total', productsCount)
+router.get('/products/:count', list)
+router.get('/product/:slug', read)
+router.put('/product/:slug', authCheck, adminCheck, update)
+router.delete('/product/:slug', authCheck, adminCheck, remove)
+
+router.post('/products/sorted', listSorted)
+
+module.exports = router
